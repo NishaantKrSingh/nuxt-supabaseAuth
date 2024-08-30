3e196df0f9753db7ec9f31a84bc4519b6229bf6b
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-08-31 04:10:20 +0530

    improved looks by adding proper css properties

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,70 @@
+<script setup>
+const login_to = ()=> {
+    return navigateTo('/login')
+}
+</script>
+
 <template>
-    <div>Index Page</div>
+    <div>
+        <div class="for-button">
+            <button type="button" @click="login_to">Login</button>
+        </div>
+        <div>
+            <h2 class="rotate">Index</h2>
+            <h2>Page</h2>
+        </div>
+    </div>
 </template>
+
+<style lang="css" scoped>
+div {
+  font-family: "didot";
+}
+
+h2 {
+  font-size: 100px;
+  margin: 0;
+   writing-mode: vertical-lr;
+  text-align: center;
+  line-height: .9;
+}
+
+.rotate {
+   transform: rotate(180deg);
+}
+
+div {
+  display: grid;
+  height: 100vh;
+  justify-content: center;
+  align-content: center;
+  grid-template-columns: max-content max-content;
+}
+
+body {
+  margin: 0;
+}
+
+.for-button {
+    position: relative;
+    flex: 1;
+}
+button {
+    position: fixed;
+    cursor: pointer;
+    top: 15px;
+    right: 15px;
+    border: none;
+    border-radius: 12px;
+    background-color: rgba(104, 85, 224, 1);
+    color: white;
+    padding: 13px 30px;
+    font-size: 18px;
+    transition-duration: 0.4s;
+}
+button:hover {
+    border: 1px solid  rgba(104, 85, 224, 1);
+    background-color: white;
+    color:  rgba(104, 85, 224, 1);
+}
+</style>
--- pages/secure.vue
+++ pages/secure.vue
@@ -9,8 +9,36 @@ const signOut = async () => {
 
 
 <template>
-    <div>secure page</div>
-    <div>
-        <button @click="signOut">Log Out</button>
+    <div class="centre">
+        <div class="space">secure page</div>
+        <button type="button" @click="signOut">Log Out</button>
     </div>
 </template>
+
+<style scoped>
+.centre {
+    text-align: center;
+    padding-top: 2rem;
+    font-size: 30px;
+}
+
+.space {
+    padding-top: 50px;
+    padding-bottom: 50px;
+}
+
+button {
+    border: none;
+    font-size: 25px;
+    background-color: rgb(39, 39, 39);
+    color: white;
+    padding: 15px 32px;
+    border-radius: 12px;
+    transition-duration: 0.4s;
+}
+button:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+}
+</style>
