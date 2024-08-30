b898adc1e2badf5bcb945928210dadb17ff1576d
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-08-31 03:09:11 +0530

    added logging in and logging out functionality

---
+++ pages/login.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const supabase = useSupabaseClient()
+const signInWithOAuth = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: 'http://localhost:3000/confirm',
+    },
+  })
+  if (error) console.log(error)
+}
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) console.log(error)
+}
+
+definePageMeta({
+  middleware: ["auth"],
+})
+</script>
+
+
+<template>
+  <div>
+    <button @click="signInWithOAuth">
+      Sign In with E-Mail
+    </button>
+  </div>
+</template>
---
+++ pages/secure.vue
@@ -0,0 +1,16 @@
+<script setup>
+const supabase = useSupabaseClient()
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) console.log(error)
+  navigateTo('/login')
+}
+</script>
+
+
+<template>
+    <div>secure page</div>
+    <div>
+        <button @click="signOut">Log Out</button>
+    </div>
+</template>
